5e1fda4643d2fae2345578310758d1b08e19cf30
spabalate <143145397+spabalate@users.noreply.github.com>
2023-11-18 13:31:01 -0800

    Added files

---
+++ Challenge_9.sql
@@ -0,0 +1,101 @@
+-- Create department table
+CREATE TABLE departments (
+	dept_no VARCHAR(10) PRIMARY KEY NOT NULL,
+	dept_name VARCHAR(30) NOT NULL
+);
+
+-- Create dept_emp table
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(10) NOT NULL
+);
+
+-- Create dept_manager table
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(10) NOT NULL,
+	emp_no INT NOT NULL
+);
+
+-- Create employees table
+CREATE TABLE employees (
+	emp_no INT PRIMARY KEY NOT NULL,
+	emp_title_id VARCHAR(10) NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	sex VARCHAR(1) NOT NULL,
+	hire_date DATE NOT NULL
+);
+
+-- Create salaries table
+CREATE TABLE salaries (
+	emp_no INT NOT NULL,
+	salary INT NOT NULL
+);
+
+-- Create titles table
+CREATE TABLE titles (
+	title_id VARCHAR(10) PRIMARY KEY NOT NULL,
+	title VARCHAR(30) NOT NULL
+);
+
+-- List the employee number, last name, first name, sex, and salary of each employee.
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+FROM employees
+JOIN salaries
+ON employees.emp_no = salaries.emp_no;
+
+-- List the first name, last name, and hire date for the employees who were hired in 1986.
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1/1/1986' AND '12/31/1986'
+ORDER BY hire_date;
+
+-- List the manager of each department along with their department number, department name, employee number, last name, and first name.
+SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
+FROM departments
+JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no
+JOIN employees
+ON dept_manager.emp_no = employees.emp_no;
+
+-- List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no;
+
+-- List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+SELECT employees.first_name, employees.last_name, employees.sex
+FROM employees
+WHERE first_name = 'Hercules'
+AND last_name Like 'B%'
+
+-- List each employee in the Sales department, including their employee number, last name, and first name.
+SELECT departments.dept_name, employees.emp_no, employees.last_name, employees.first_name
+FROM dept_emp
+JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales';
+
+-- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT departments.dept_name, employees.emp_no, employees.last_name, employees.first_name
+FROM dept_emp
+JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales'
+OR departments.dept_name = 'Development';
+
+-- List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+SELECT last_name,
+COUNT(last_name) AS "frequency"
+FROM employees
+GROUP BY last_name
+ORDER BY
+COUNT(last_name) DESC;
